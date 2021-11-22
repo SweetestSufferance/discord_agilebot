59f7234da6f095a247c41fdd7beb634015430c33
Jeremy Sichak <jeremy.sichak@outlook.com>
2021-11-22 10:08:14 -0600

    added classes to simplify code

--- agilebot.py
+++ agilebot.py
@@ -3,9 +3,11 @@ import redis
 import os
 import boto3
 import time
+import codetiming
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from codetiming import Timer
 
 load_dotenv()
 
@@ -17,121 +19,96 @@ AUTH_TOKEN = str(redis_server.get('AUTH_TOKEN').decode('utf-8'))
 bot = commands.Bot(command_prefix='!')
 
 class get_resource:
-    def __init__(self, resource, instanceid)
+    def __init__(self, resource, instanceid):
         self.ec2 = boto3.resource(resource)
         self.instance = self.ec2.Instance(instanceid)
 
 class get_state:
     def __init__(self, resource, instanceid):
         self.name = 'unavailable'
-        self.instance = get_resource(resource, instanceid)
+        self.instance = get_resource(resource, instanceid).instance
         self.name = self.instance.state['Name'].lower()
 
 class get_publicip:
     def __init__(self, resource, instanceid):
-        self.instance = get_resource(resource, instanceid)
+        self.instance = get_resource(resource, instanceid).instance
         if get_state(resource, instanceid).name == 'running':
-            self.publicip = self.instance.
+            self.ip = self.instance.public_ip_address
+        else:
+            self.ip = 'no ip'
 
 class invoke_stop:
     def __init__(self, resource, instanceid):
-        self.instance = get_resource(resource, instanceid)
+        self.instance = get_resource(resource, instanceid).instance
         if get_state(resource, instanceid).name == 'running':
             self.instance.stop()
-            while get_state(resource, instanceid).name != 'stopped'
-            sleep 10
+            while get_state(resource, instanceid).name != 'stopped' or get_state(resource,instanceid).name != 'stopping':
+                time.sleep(10)
 
-class invoke_restart
+class invoke_restart:
+    print(f"not available")
 
-class invoke_update
-
-
-def getip(server,instance_id):
-    ec2 = boto3.resource('ec2')
-    instance = ec2.Instance(instance_id)
-    if instance.state['Name'].lower() != 'running':
-        await ctx.send(server + "is not running, please start the server")
-        return
-    public_ip = instance.public_ip_address
-    await ctx.send(public_ip)
-
-def status(server,instance_id)
-
-def start(server,instance_id)
-
-def stop(server,instance_id)
-
-def restart(server,instance_id)
-
-def update(server.instance_id)
+class invoke_update:
+    print(f"not available")
 
 @bot.command(name='chode', help='testing multiple args', pass_context=True)
 async def chode(ctx, server, command):
     server = server.lower()
     command = command.lower()
     if server == 'satisfactory':
-        ec2 = boto3.resource('ec2')
-        instance = ec2.Instance('<instanceid>')
+        instanceid = '<instanceid>'
 
         if command == 'getip':
-            if instance.state['Name'] != 'running':
+            if get_state('ec2', instanceid).name != 'running':
                 await ctx.send("Satisfactory server is not running, please start the server")
                 return
-
-            publicip = instance.public_ip_address
-            await ctx.send(publicip)
+            await ctx.send(get_publicip('ec2', instanceid).ip)
 
         elif command == 'start':
-            if instance.state['Name'] == 'stopped':
-                instance.start()
-                while instance.state['Name'] != 'running':
-                    instance = ec2.Instance('<instanceid>')
-                    await ctx.send("Current Satisfactory state is:  "+instance.state['Name'])
+            if get_state('ec2', instanceid).name == 'stopped':
+                get_resource('ec2', instanceid).instance.start()
+                await ctx.send("Current Satisfactory state is: " + get_state('ec2', instanceid).name)
+                while get_state('ec2', instanceid) != 'running':
+                    if get_state('ec2', instanceid).name == 'running':
+                        break
                     time.sleep(10)
-            elif instance.state['Name'] == 'running':
-                await ctx.send("Satisfactory server state is "+instance.state['Name']+", there is no need to start the server")
-            await ctx.send("Current Satisfactory IP is:  "+instance.public_ip_address)
+
+            await ctx.send("Current Satisfactory IP is:  " + get_publicip('ec2', instanceid).ip)
 
         elif command == 'status':
-            await ctx.send("Current Satisfactory state is:  "+instance.state['Name'])
+            await ctx.send("Current Satisfactory state is:  " + get_state('ec2', instanceid).name)
 
         elif command == 'restart':
             if ctx.message.author == client.user:
                 return
-            instance = ec2.Instance('<instanceid>')
-            if ctx.message.author.mention == '<username>':
-                if instance.state['Name'] != 'running':
-                    await ctx.send("Please start the server before issuing a restart command")
-                    return
-                instance.restart()
-                await ctx.send("Waiting for system to restart")
-                while instance.state['Name'] != 'running':
-                    instance = ec2.Instance('<instanceid>')
-                    await ctx.send("Current instance state is:  "+instance.state['Name']+", waiting for server to start")
+            if get_state('ec2', instanceid) == 'running':
+                get_resource('ec2', instanceid).restart()
+                time.sleep(5)
+                while get_state('ec2', instanceid) != 'running':
                     time.sleep(10)
-            elif instance.state['Name'] == 'running':
-                await ctx.send("Satisfactory server state is "+instance.state['Name']+", there is no need to start the server")
-            elif instance.state['Name'] == 'stopped' or instance.state['Name'] == 'stopping':
-                await ctx.send("Satisfactory server is in a stopped or stopping state, please issue a start command")
-                return
-            await ctx.send("Current Satisfactory IP is:  "+instance.public_ip_address)
+            await ctx.send("Current server state is: " + get_state('ec2', instanceid).name)
+            await ctx.send("Current Satisfactory IP is:  "+ get_publicip('ec2', instanceid).ip)
 
+        elif command == 'stop':
+            if get_state('ec2', instanceid).name == 'running':
+                get_resource('ec2', instanceid).instance.stop()
+                await ctx.send("Current Satisfactory state is: " + get_state('ec2', instanceid).name)
+                while get_state('ec2', instanceid) != 'stopped':
+                    if get_state('ec2', instanceid).name == 'stopped':
+                        break
+                    time.sleep(10)
+            
+            await ctx.send("Satisfactory server state is " + get_state('ec2', instanceid).name)
+            
     elif server == 'botserver':
         if command == 'getip':
-            ec2 = boto3.resource('ec2')
-            instance = ec2.Instance('<instanceid>')
-            publicip = instance.public_ip_address
-            await ctx.send(ctx.guild.roles)
-            await ctx.send(discord.Member.roles)
-            await ctx.send(publicip)
+            instanceid = '<instanceid>'
+            await ctx.send(get_publicip('ec2', instanceid).ip)
 
     elif server == 'valheim':
         if command == 'getip':
-            print(command)
-            ec2 = boto3.resource('ec2')
-            instance = ec2.Instance('')
-            publicip = instance.public_ip_address
-            await ctx.send(publicip)
+            instanceid = '<instanceid>'
+            await ctx.send(get_publicip('ec2', instanceid).ip)
 
 bot.run(AUTH_TOKEN) # Pull Auth Token from above
 
